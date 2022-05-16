23ca1f2271b4344f3b4c8cb7a40c0390ebe79100
ToanNPT <thanhtoannguyen08102001@gmail.com>
2022-05-16 23:41:18 +0700

    add image to cart and fix some bugs in home

--- src/components/session/product/product.component.jsx
+++ src/components/session/product/product.component.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import productApi from "../../../api/productApi";
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import {showNotification, checkQuantity} from '../../../utils/MyUtils';
+import { Link, useNavigate } from "react-router-dom";
 import Rating from "@material-ui/lab/Rating";
 import Box from "@material-ui/core/Box";
-
+import { useSelector, useDispatch } from "react-redux";
+import { insertCartRedux } from "../../../redux/cartRedux";
 function Product() {
+
+  const user = useSelector(state => state.user.currentUser);
+  const listCartRedux = useSelector(state => state.cart.listCart);
+  const dispatch = useDispatch();
   const [value, setValue] = useState(4);
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -67,6 +74,41 @@ function Product() {
     }
   };
 
+  const addToCart = async (e, item) => {
+    e.preventDefault();
+    if (!user) {
+      navigate('/signin')
+    }
+    else {
+      if (item.quantity === 0) {
+        showNotification("warning", "HUHU OH NO !!!", "Not enough products, my friends", "Choose others");
+      }
+      else {
+        const cartItem = {
+          id: {
+            username: user.username,
+            productId: item.productId,
+          },
+          quantity: 1,
+        };
+        const resultCheck = await checkQuantity(cartItem, item.quantity, listCartRedux);
+        if (resultCheck) {
+          const res = await insertCartRedux(dispatch, cartItem, user.username, item.productId);
+          console.log(res)
+          if (res === 200) {
+            showNotification("success", "Great", "Add to cart ssuccess", 'Ok');
+          }
+          else
+            showNotification("error", "Oh No", "Not enough, try again", "Ok");
+        }
+        else {
+          showNotification("error", "Oh No", "Not enough, try again", "Ok");
+        }
+      }
+    }
+
+  };
+
   return (
     <section className="product spad">
       <div className="container">
@@ -95,131 +137,131 @@ function Product() {
         <div className="row property__gallery">
           {filteredProducts.length > 0
             ? filteredProducts.map((product) => (
-                <div
-                  key={product.id}
-                  className="col-lg-3 col-md-4 col-sm-6 mix women"
-                >
-                  <div className="product__item">
-                    <div
-                      className="product__item__pic set-bg"
-                      data-setbg={product.image}
-                      // set style background-image
-                      style={{ backgroundImage: `url(${product.image})` }}
-                    >
-                      <div className="label new">New</div>
-                      <ul className="product__hover">
-                        <li>
-                          <a href={product.image} className="image-popup">
-                            <span className="arrow_expand" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <span className="icon_heart_alt" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <span className="icon_bag_alt" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="product__item__text">
-                      <h6>
-                        <Link to={`/product/${product.productId}`}>
-                          {product.name}
+              <div
+                key={product.id}
+                className="col-lg-3 col-md-4 col-sm-6 mix women"
+              >
+                <div className="product__item">
+                  <div
+                    className="product__item__pic set-bg"
+                    data-setbg={product.image}
+                    // set style background-image
+                    style={{ backgroundImage: `url(${product.image})` }}
+                  >
+                    <div className="label new">New</div>
+                    <ul className="product__hover">
+                      <li>
+                        <a href={product.image} className="image-popup">
+                          <span className="arrow_expand" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <span className="icon_heart_alt" />
+                        </a>
+                      </li>
+                      <li>
+                        <Link onClick={(e) => (e)} to='/cart'>
+                          <span className="icon_bag_alt" />
                         </Link>
-                      </h6>
-                      {/* <div className="rating">
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="product__item__text">
+                    <h6>
+                      <Link to={`/product/${product.productId}`}>
+                        {product.name}
+                      </Link>
+                    </h6>
+                    {/* <div className="rating">
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                     </div> */}
-                      <Box
-                        component="fieldset"
-                        mb={3}
-                        borderColor="transparent"
-                      >
-                        <Rating name="read-only" value={value} readOnly />
-                      </Box>
-                      <div className="product__price">
-                        {product && product.price
-                          ? product.price.toLocaleString("it-IT", {
-                              style: "currency",
-                              currency: "VND",
-                            })
-                          : null}
-                      </div>
+                    <Box
+                      component="fieldset"
+                      mb={3}
+                      borderColor="transparent"
+                    >
+                      <Rating name="read-only" value={value} readOnly />
+                    </Box>
+                    <div className="product__price">
+                      {product && product.price
+                        ? product.price.toLocaleString("it-IT", {
+                          style: "currency",
+                          currency: "VND",
+                        })
+                        : null}
                     </div>
                   </div>
                 </div>
-              ))
+              </div>
+            ))
             : products.map((product) => (
-                <div
-                  key={product.id}
-                  className="col-lg-3 col-md-4 col-sm-6 mix women"
-                >
-                  <div className="product__item">
-                    <div
-                      className="product__item__pic set-bg"
-                      data-setbg={product.image}
-                      // set style background-image
-                      style={{ backgroundImage: `url(${product.image})` }}
-                    >
-                      <div className="label new">New</div>
-                      <ul className="product__hover">
-                        <li>
-                          <a href={product.image} className="image-popup">
-                            <span className="arrow_expand" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <span className="icon_heart_alt" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <span className="icon_bag_alt" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="product__item__text">
-                      <h6>
-                        <Link to={`/product/${product.productId}`}>
-                          {product.name}
+              <div
+                key={product.id}
+                className="col-lg-3 col-md-4 col-sm-6 mix women"
+              >
+                <div className="product__item">
+                  <div
+                    className="product__item__pic set-bg"
+                    data-setbg={product.image}
+                    // set style background-image
+                    style={{ backgroundImage: `url(${product.image})` }}
+                  >
+                    <div className="label new">New</div>
+                    <ul className="product__hover">
+                      <li>
+                        <a href={product.image} className="image-popup">
+                          <span className="arrow_expand" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <span className="icon_heart_alt" />
+                        </a>
+                      </li>
+                      <li>
+                        <Link onClick = {(e) => addToCart(e, product)} to="/cart">
+                          <span className="icon_bag_alt" />
                         </Link>
-                      </h6>
-                      {/* <div className="rating">
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="product__item__text">
+                    <h6>
+                      <Link to={`/product/${product.productId}`}>
+                        {product.name}
+                      </Link>
+                    </h6>
+                    {/* <div className="rating">
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                       <i className="fa fa-star" />
                     </div> */}
-                      <Box
-                        component="fieldset"
-                        mb={3}
-                        borderColor="transparent"
-                      >
-                        <Rating name="read-only" value={value} readOnly />
-                      </Box>
-                      <div className="product__price">
-                        {product && product.price
-                          ? product.price.toLocaleString("it-IT", {
-                              style: "currency",
-                              currency: "VND",
-                            })
-                          : null}
-                      </div>
+                    <Box
+                      component="fieldset"
+                      mb={3}
+                      borderColor="transparent"
+                    >
+                      <Rating name="read-only" value={value} readOnly />
+                    </Box>
+                    <div className="product__price">
+                      {product && product.price
+                        ? product.price.toLocaleString("it-IT", {
+                          style: "currency",
+                          currency: "VND",
+                        })
+                        : null}
                     </div>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))}
         </div>
       </div>
     </section>
